3883b7720ced15fe255462015f732b0697e03aa2
Kaya98 <74619525+Kaya98@users.noreply.github.com>
2022-07-19 16:59:17 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -13,13 +13,51 @@
 
 namespace chrono = std::chrono;
 
-void naive_matrix_multiply(float *a, float *b, float *c, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            c[i * n + j] = 0.0f;
-            for (int k = 0; k < n; k++) {
-                c[i * n + j] += a[i * n + k] * b[k * n + j];
+void transpose_matrix(float* m, int n){
+    int k = (n * (n-1)) / 2;
+    int start = 1;
+    int plus = 2;
+    int j = 0;
+    int line = 0;
+    float t = 0;
+    for (int i = 0; i< k; i++){
+        t = m[start + i];
+        m[start + i] = m[(j+1)*n + line];
+        m[(j+1)*n + line] = t;
+        j++;
+        if((start + i)% n == (n-1)){
+            start = start + plus;
+            plus++;
+            line++;
+            j = line;
+        }
+    }
+}
+
+void final_multi_thread_task(float *a, float *b, float *c, int i, int n){
+    __m128 temp = _mm_set1_ps(0);
+    __m128 am = _mm_set1_ps(0);
+    __m128 bm = _mm_set1_ps(0);
+    for (int j = 0; j < n; j++) {
+            temp = _mm_set1_ps(0);
+            for (int k = 0; k < n; k= k + 4) {
+                am = _mm_load_ps(&a[i * n + k]);
+                bm = _mm_load_ps(&b[j * n + k]);
+                temp = _mm_add_ps(temp, _mm_mul_ps(am, bm));
             }
+            c[i * n + j] = temp[0] + temp[1] + temp[2] + temp[3];
+        }
+}
+
+void naive_matrix_multiply(float *a, float *b, float *c, int n) {
+    transpose_matrix(b, n);
+    for (int i = 0; i < n; i= i + 4) {
+        std::thread th[4];
+        for(int t= i; t<i+4; t++){
+            th[t-i]= std::thread(final_multi_thread_task, a, b, c, t, n);
+        }
+        for(int t= 0; t<4; t++){
+            th[t].join();
         }
     }
 }
